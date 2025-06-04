cb9def9a032ed27cd18945c668bff73c09bf6cda
Krypto1910 <jackx607@gmail.com>
2025-06-04 12:29:51 +0700

    eidt search box, apply panel

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,22 +1,29 @@
  <template>
   <div id="app" class="flex flex-col min-h-screen">
     <Navbar />
-    <main class="flex-grow">
+    <!-- <main class="flex-grow">
       <RouterView />
-    </main>
+    </main> -->
+    <Home />
     <PageFooter />
   </div>
 </template>
 
 
 <script>
+// import { RouterView } from 'vue-router'
 import Navbar from './components/Navbar.vue'
 import PageFooter from './components/PageFooter.vue'
+import Home from './components/Home.vue'
+
+
+
 
 export default {
   name: 'App',
   components: {
     Navbar,
+    Home,
     PageFooter
   }
 }
--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -15,7 +15,15 @@
                 <div class="col-4 d-flex justify-content-center">
                     <form class="d-flex" style="max-width: 100%; width: 100%;">
                         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                        <button style="color: white;" class="btn btn-outline-success" type="submit">Search</button>
+                        <button style="color: white;" class="btn btn-outline-success" type="submit">
+
+                            <svg xmlns="http://www.w3.org/2000/svg"  width="20" height="24" fill="currentColor"
+                                class="bi bi-search" viewBox="0 0 16 16">
+                                <path
+                                    d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
+                            </svg>
+
+                        </button>
                     </form>
                 </div>
 
@@ -78,7 +86,6 @@ export default {
 .navbar .nav-link {
     font-weight: 600;
 }
-
 
 .site-footer {
     background-color: #1a1a1a;
