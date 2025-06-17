03d1396aeafae1a77cc23907d06cfbcb9ac24119
Krypto1910 <jackx607@gmail.com>
2025-06-17 13:21:27 +0700

    add list item and fix size

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -1,24 +1,218 @@
 <template>
-    <!--Panel-->
-    <section class="content" >
+    <!--Banner-->
+    <div class="content">
         <div class="banner-container">
             <img :src="banner" alt="Banner Image">
         </div>
-        <div class="items" style="background-color: aquamarine; width: 100%; height: 100%; padding: 20px; border-radius: 10px; margin-top: 50px;">
-            <h1>Welcome to NeoStyle</h1>
-            <p>Your one-stop shop for the latest fashion trends.</p>
+        <div id="featured" class="text-center my-5">
+                <h3>Best sale</h3>
+                <hr class="mx-auto">
+                <p>Check out our luxury watches</p>
+            </div>
+        <div class="product-container">     
+            <div class="row mx-auto container-fluid">
+                <div onclick="window.location.href='single_product.html';"
+                    class="product text-center col-lg-3 col-md-4 col-sm-12">
+                    <img :src="watch1" alt="">
+                    <div class="star">
+                        <i class="far fa-star"></i>
+                        <i class="far fa-star"></i>
+                        <i class="far fa-star"></i>
+                        <i class="far fa-star"></i>
+                        <i class="fas fa-star"></i>
+                    </div>
+                    <h5 class="p-name">Watch 1</h5>
+                    <h4 class="p-price">$999.9</h4>
+                    <button class="buy-btn">Buy Now</button>
+                </div>
+
+                <div onclick="window.location.href='single_product.html';"
+                    class="product text-center col-lg-3 col-md-4 col-sm-12">
+                    <img :src="watch2" alt="">
+                    <div class="star">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                    </div>
+                    <h5 class="p-name">Watch 2</h5>
+                    <h4 class="p-price">$999.9</h4>
+                    <button class="buy-btn">Buy Now</button>
+                </div>
+
+                <div onclick="window.location.href='single_product.html';"
+                    class="product text-center col-lg-3 col-md-4 col-sm-12">
+                    <img :src="watch3" alt="">
+                    <div class="star">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                    </div>
+                    <h5 class="p-name">Watch 3</h5>
+                    <h4 class="p-price">$999.9</h4>
+                    <button class="buy-btn">Buy Now</button>
+                </div>
+
+                <div onclick="window.location.href='single_product.html';"
+                    class="product text-center col-lg-3 col-md-4 col-sm-12">
+                    <img :src="watch4" alt="">
+                    <div class="star">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                    </div>
+                    <h5 class="p-name">Watch 4</h5>
+                    <h4 class="p-price">$999.9</h4>
+                    <button class="buy-btn">Buy Now</button>
+                </div>
+            </div>
+            <div class="row mx-auto container-fluid">
+                <div onclick="window.location.href='single_product.html';"
+                    class="product text-center col-lg-3 col-md-4 col-sm-12">
+                    <img :src="watch1" alt="">
+                    <div class="star">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                    </div>
+                    <h5 class="p-name">Watch 1</h5>
+                    <h4 class="p-price">$999.9</h4>
+                    <button class="buy-btn">Buy Now</button>
+                </div>
+
+                <div onclick="window.location.href='single_product.html';"
+                    class="product text-center col-lg-3 col-md-4 col-sm-12">
+                    <img :src="watch2" alt="">
+                    <div class="star">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                    </div>
+                    <h5 class="p-name">Watch 2</h5>
+                    <h4 class="p-price">$999.9</h4>
+                    <button class="buy-btn">Buy Now</button>
+                </div>
+
+                <div onclick="window.location.href='single_product.html';"
+                    class="product text-center col-lg-3 col-md-4 col-sm-12">
+                    <img :src="watch3" alt="">
+                    <div class="star">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                    </div>
+                    <h5 class="p-name">Watch 3</h5>
+                    <h4 class="p-price">$999.9</h4>
+                    <button class="buy-btn">Buy Now</button>
+                </div>
+
+                <div onclick="window.location.href='single_product.html';"
+                    class="product text-center col-lg-3 col-md-4 col-sm-12">
+                    <img :src="watch4" alt="">
+                    <div class="star">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                    </div>
+                    <h5 class="p-name">Watch 4</h5>
+                    <h4 class="p-price">$999.9</h4>
+                    <button class="buy-btn">Buy Now</button>
+                </div>
+            </div>
+            <div class="row mx-auto container-fluid">
+                <div onclick="window.location.href='single_product.html';"
+                    class="product text-center col-lg-3 col-md-4 col-sm-12">
+                    <img :src="watch1" alt="">
+                    <div class="star">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                    </div>
+                    <h5 class="p-name">Watch 1</h5>
+                    <h4 class="p-price">$999.9</h4>
+                    <button class="buy-btn">Buy Now</button>
+                </div>
+
+                <div onclick="window.location.href='single_product.html';"
+                    class="product text-center col-lg-3 col-md-4 col-sm-12">
+                    <img :src="watch2" alt="">
+                    <div class="star">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                    </div>
+                    <h5 class="p-name">Watch 2</h5>
+                    <h4 class="p-price">$999.9</h4>
+                    <button class="buy-btn">Buy Now</button>
+                </div>
+
+                <div onclick="window.location.href='single_product.html';"
+                    class="product text-center col-lg-3 col-md-4 col-sm-12">
+                    <img :src="watch3" alt="">
+                    <div class="star">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                    </div>
+                    <h5 class="p-name">Watch 3</h5>
+                    <h4 class="p-price">$999.9</h4>
+                    <button class="buy-btn">Buy Now</button>
+                </div>
+
+                <div onclick="window.location.href='single_product.html';"
+                    class="product text-center col-lg-3 col-md-4 col-sm-12">
+                    <img :src="watch4" alt="">
+                    <div class="star">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                    </div>
+                    <h5 class="p-name">Watch 4</h5>
+                    <h4 class="p-price">$999.9</h4>
+                    <button class="buy-btn">Buy Now</button>
+                </div>
+            </div>
         </div>
-    </section>
-    
+
+    </div>
 </template>
 
 <script>
 import banner from '@/assets/banner.jpeg';
+import watch1 from '@/assets/watch1.jpeg';
+import watch2 from '@/assets/watch2.jpeg';
+import watch3 from '@/assets/watch3.jpeg';
+import watch4 from '@/assets/watch4.jpeg';
 export default {
     name: 'Home',
     data() {
         return {
-            banner
+            banner,
+            watch1,
+            watch2,
+            watch3,
+            watch4
         };
     }
 }
@@ -28,27 +222,109 @@ export default {
 .content {
     display: flex;
     flex-direction: column;
-    background-color: white; 
-    box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.1);
-    color:  black;
+    background-color: transparent;
+    color: black;
     width: 70%;
     height: fit-content;
     margin: 3% auto;
-    border-radius: 10px;
-    border-bottom-left-radius: 0;
-    border-bottom-right-radius: 0;
+    margin-top: 137rem;
 }
+
+/* banner */
 .banner-container {
     width: 100%;
     height: 30%;
-    border-radius: 10px;
 }
+
 img {
     width: 100%;
-    height: 350px;
+    height: 28rem;
     object-fit: cover;
     border-radius: 10px;
 }
 
+/* products */
+.product-container {
+    width: 100%;
+    height: fit-content;
+    background-color: white;
+    /* Thêm khoảng cách bên trong */
+}
+.row.mx-auto.container-fluid {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    /* Chia đều các phần tử */
+    margin: 3.5rem 0;
+}
 
+.product {
+    flex: 1;
+    /* Chia đều không gian */
+    max-width: 25%;
+    /* Mỗi sản phẩm chiếm 25% chiều rộng */
+    padding: 10px;
+    /* Thêm khoảng cách giữa các sản phẩm */
+    box-sizing: border-box;
+    cursor: pointer;
+    margin-bottom: 1.5rem;
+    margin-top: 1.5rem;
+}
+
+.star i {
+    color: goldenrod;
+    margin: 10px 0;
+    font-size: 1rem;
+}
+
+.product img {
+    width: 80%;
+    /* Đảm bảo ảnh đầy đủ chiều ngang */
+    height: 28.3rem;
+    /* Đặt chiều cao cố định */
+    object-fit: cover;
+    /* Đảm bảo hình ảnh không bị méo */
+    transition: 0.3s all;
+
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+}
+
+.product:hover img {
+    opacity: 0.8;
+}
+
+.product .buy-btn {
+    color: white;
+    background-color: black;
+    padding: 10px 30px;
+    transform: translateY(30px);
+    opacity: 0;
+    transition: 0.3s all;
+    font-size: 1.3rem;
+}
+
+.product:hover .buy-btn {
+    transform: translateY(0px);
+    opacity: 1;
+}
+
+hr {
+    width: 30px;
+    height: 3px !important;
+    opacity: 1 !important;
+    background-color: burlywood;
+    border: none;
+}
+
+h3 {
+    font-weight: 600;
+    font-size: 2.3rem;
+    color: rgb(0, 0, 0, 1);
+}
+
+p {
+    font-weight: 600;
+    font-size: 1.2rem;
+    color: rgb(0, 0, 0, 0.6);
+}
 </style>
--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -5,25 +5,27 @@
         <div class="container-fluid" style="padding-right: 0px;">
             <div class="row w-100 align-items-center">
                 <!-- Logo bên trái -->
-                <div class="col-4 d-flex align-items-center" style="padding-left: 5%">
+                <div class="col-4 d-flex align-items-center" style="padding-left: 5%; ">
                     <img class="logo me-2" :src="logoUrl" alt="NeoStyle Logo"
-                        style="width: 40px; height: 40px; background-color: white;">
-                    <h2 style="color: white;" class="brand mb-0">NeoStyle</h2>
+                        style="width: 4rem; height: auto; background-color: white;">
+                    <h2 style="color: white; font-size: 2.3rem;" class="brand mb-0">NeoStyle</h2>
                 </div>
 
                 <!-- Search box giữa -->
                 <div class="col-4 d-flex justify-content-center">
-                    <form class="d-flex" style="max-width: 100%; width: 100%;">
-                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                        <button style="color: white;" class="btn btn-outline-success" type="submit">
+                    <form class="d-flex" style="max-width: 100%; width: 100%; margin-left: 15%;">
+                        <div style="color: rgb(0, 0, 0, 0.5); font-size: 1.3rem; cursor: auto;" class="search-icon"
+                            type="button">
 
-                            <svg xmlns="http://www.w3.org/2000/svg"  width="20" height="24" fill="currentColor"
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="24" fill="currentColor"
                                 class="bi bi-search" viewBox="0 0 16 16">
                                 <path
                                     d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
                             </svg>
 
-                        </button>
+                        </div>
+                        <input style=" font-size: 1.3rem ; " class="form-control me-2" type="search" placeholder="Search"
+                            aria-label="Search">
                     </form>
                 </div>
 
@@ -33,15 +35,15 @@
                         <ul class="navbar-nav align-items-center gap-1 flex-row mb-0">
                             <li class="nav-item d-flex gap-1">
                                 <a class="nav-link d-flex align-items-center" href="#" style="padding: 10px 16px;">
-                                    <svg xmlns="http://www.w3.org/2000/svg" height="20" viewBox="0 0 576 512"
-                                        fill="currentColor">
+                                    <svg style="font-size: 10rem;" xmlns="http://www.w3.org/2000/svg" height="20"
+                                        viewBox="0 0 576 512" fill="currentColor">
                                         <path
                                             d="M24 0C10.7 0 0 10.7 0 24S10.7 48 24 48l45.5 0c3.8 0 7.1 2.7 7.9 6.5l51.6 271c6.5 34 36.2 58.5 70.7 58.5L488 384c13.3 0 24-10.7 24-24s-10.7-24-24-24l-288.3 0c-11.5 0-21.4-8.2-23.6-19.5L170.7 288l288.5 0c32.6 0 61.1-21.8 69.5-53.3l41-152.3C576.6 57 557.4 32 531.1 32l-411 0C111 12.8 91.6 0 69.5 0L24 0zM131.1 80l389.6 0L482.4 222.2c-2.8 10.5-12.3 17.8-23.2 17.8l-297.6 0L131.1 80zM176 512a48 48 0 1 0 0-96 48 48 0 1 0 0 96zm336-48a48 48 0 1 0 -96 0 48 48 0 1 0 96 0z" />
                                     </svg>
                                     <span>Cart</span>
                                 </a>
 
-                                <a class="nav-link" href="#" style="padding: 10px 16px;">
+                                <a class="nav-link" href="#" style="padding: 10px 16px; margin-left: 1%;">
                                     <i class="fas fa-user"></i>
                                     <span>Account</span>
                                 </a>
@@ -75,16 +77,12 @@ export default {
     color: white;
     border: 2px solid white;
     font-weight: 600;
-
 }
 
-.navbar .btn:hover {
-    background-color: bisque;
-    color: black !important;
-}
 
-.navbar .nav-link {
+.navbar .nav-link span {
     font-weight: 600;
+    font-size: 1.35rem;
 }
 
 .site-footer {
@@ -93,6 +91,31 @@ export default {
     padding: 20px;
     text-align: center;
     width: 100%;
+}
+
+/*search box*/
+.navbar input[type="search"] {
+    border-radius: 30px;
+    border: none;
+    outline: none;
+    font-size: 16px;
+    border-top-left-radius: 0;
+    border-bottom-left-radius: 0;
+    margin: 0px !important;
+    width: 80%;
+}
+
+.navbar .search-icon {
+    background-color: white;
+    border-radius: 30px;
+    border-top-right-radius: 0;
+    border-bottom-right-radius: 0;
+    border: 5px solid white;
+    padding: 5px 0px 8px 15px;
+}
+
+.navbar{
+    padding: 20px 0 !important;
 }
 
 
