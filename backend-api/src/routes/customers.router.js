65a8ecbfc089ce82d465635dae04b9980f4cddc3
Morning-Gloryy <lhlt452004@gmail.com>
2025-07-25 04:07:41 +0700

    Add rate limnit for login and register user

--- backend-api/src/routes/customers.router.js
+++ backend-api/src/routes/customers.router.js
@@ -6,7 +6,12 @@ const { customersSchema
 const { z } = require('zod');
 const multer = require("multer");
 const upload = multer(); 
-
+const rateLimit = require('express-rate-limit');
+const limiter = rateLimit({
+  windowMs: 60 * 1000,
+  max: 5,
+  message: { status: "error", message: "Too many attempts" }
+});
 
 const router = express.Router();
 const { validateRequest } = require('../middlewares/validator.middleware')
@@ -14,10 +19,11 @@ const { validateRequest } = require('../middlewares/validator.middleware')
 module.exports.setup = (app) => {
   app.use('/api/v1/customers',upload.none(), router);
 
-  router.post('/register', customersController.createCustomer);
+  router.post('/register',limiter, customersController.createCustomer);
 
   router.post(
     '/login',
+    limiter,
     validateRequest(
       z.object({
         input: customersSchema.pick({ email: true, password: true }).strict()
